44319a0302e69d61a634fa7723ffc8d28ea08a59
José Bauer <jose.h.bauer03@gmail.com>
2025-02-26 14:38:06 -0300

    Altera estilos e atualiza o Favicon

--- src/components/technologiesSection/TechnologiesSection.tsx
+++ src/components/technologiesSection/TechnologiesSection.tsx
@@ -10,35 +10,35 @@ export default function TechnologiesSection() {
         <div className={styles.cardTech}>
           <p className={styles.description}>Essas são as tecnologias com as quais tive mais experiência e maior domínio, mas estou sempre aberto a aprender novas ferramentas conforme a necessidade.</p>
           <div className={styles.techIcons}>
-            <div className="d-flex flex-column align-items-center gap-2">
+            <div className={styles.techIcon}>
               <Image src="/icons/technologies/nextjs-icon.svg" alt="Next.js" width={40} height={40} />
               <p>Next.js</p>
             </div>
-            <div className="d-flex flex-column align-items-center gap-2">
+            <div className={styles.techIcon}>
               <Image src="/icons/technologies/logo-react.svg" alt="React" width={40} height={40} />
               <p>React</p>
             </div>
-            <div className="d-flex flex-column align-items-center gap-2">
+            <div className={styles.techIcon}>
               <Image src="/icons/technologies/logo-sass.svg" alt="Sass" width={40} height={40} />
               <p>Sass</p>
             </div>
-            <div className="d-flex flex-column align-items-center gap-2">
+            <div className={styles.techIcon}>
               <Image src="/icons/technologies/typescript.svg" alt="TypeScript" width={40} height={40} />
               <p>TypeScript</p>
             </div>
-            <div className="d-flex flex-column align-items-center gap-2">
+            <div className={styles.techIcon}>
               <Image src="/icons/technologies/logo-js.svg" alt="JavaScript" width={40} height={40} />
               <p>JavaScript</p>
             </div>
-            <div className="d-flex flex-column align-items-center gap-2">
+            <div className={styles.techIcon}>
               <Image src="/icons/technologies/node-js-icon.svg" alt="Node.js" width={40} height={40} />
               <p>Node.js</p>
             </div>
-            <div className="d-flex flex-column align-items-center gap-2">
+            <div className={styles.techIcon}>
               <Image src="/icons/technologies/postgresql.svg" alt="PostgreSQL" width={40} height={40} />
               <p>PostgreSQL</p>
             </div>
-            <div className="d-flex flex-column align-items-center gap-2">
+            <div className={styles.techIcon}>
               <Image src="/icons/technologies/git-logo.svg" alt="Git" width={40} height={40} />
               <p>GIT</p>
             </div>
